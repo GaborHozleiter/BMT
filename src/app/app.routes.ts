c24ce794bf4dc2c46abbd732a65f511bb8f5b7c9
Gabor Hozleiter <hozleitergabor@gmail.com>
2024-11-05 22:37:42 +0100

    added login,signin and dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LogInComponent } from './auth/log-in/log-in.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'logIn', component : LogInComponent, pathMatch: 'full' },
+    { path: 'signIn', component : SignInComponent },
+    { path: 'dashboard', component : DashboardComponent },
+];
